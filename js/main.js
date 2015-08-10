f3a7832fadf474cee1e0c7af9ab8c7777d2a7711
Miguel <migueog@gmail.com>
2015-08-10 05:33:27 -0400

    Many fixes
    - Removed parallax
    - Mobile friendly
    - Navbar scrolls around

--- js/main.js
+++ js/main.js
@@ -1,42 +1,29 @@
-var canvas = document.getElementById('canvas'),
-			context = canvas.getContext('2d'),
-			img = new Image(),
-			w, h, offset, glitchInterval;
+		$(document).ready(function () {
+			$("#homeButton").click(function () {
+				//$(this).animate(function(){
+				$('html, body').animate({
+					scrollTop: $("#main").offset().top
+				}, 1000);
+				//});
+			});
+		});
 
-			img.src = 'img/BG.png';
-			img.onload = function () {
-				init();
-				window.onresize = init;
-			};
+$(document).ready(function () {
+			$("#aboutButton").click(function () {
+				//$(this).animate(function(){
+				$('html, body').animate({
+					scrollTop: $("#about").offset().top
+				}, 1000);
+				//});
+			});
+		});
 
-		var init = function () {
-			clearInterval(glitchInterval);
-			canvas.width = w = window.innerWidth;
-			offset = w * .1;
-			canvas.height = h = ~~(175 * ((w - (offset * 2)) / img.width));
-			glitchInterval = setInterval(function () {
-				clear();
-				context.drawImage(img, 0, 110, img.width, 225, offset, 0, w - (offset * 2), h);
-				setTimeout(glitchImg, randInt(250, 1000));
-			}, 500);
-		};
-
-		var clear = function () {
-			context.rect(0, 0, w, h);
-			context.fill();
-		};
-
-		var glitchImg = function () {
-			for (var i = 0; i < randInt(1, 13); i++) {
-				var x = Math.random() * w;
-				var y = Math.random() * h;
-				var spliceWidth = w - x;
-				var spliceHeight = randInt(5, h / 3);
-				context.drawImage(canvas, 0, y, spliceWidth, spliceHeight, x, y, spliceWidth, spliceHeight);
-				context.drawImage(canvas, spliceWidth, y, x, spliceHeight, 0, y, x, spliceHeight);
-			}
-		};
-
-		var randInt = function (a, b) {
-			return ~~(Math.random() * (b - a) + a);
-		};
+		$(document).ready(function () {
+			$("#contactButton").click(function () {
+				//$(this).animate(function(){
+				$('html, body').animate({
+					scrollTop: $("#contact").offset().top
+				}, 1000);
+				//});
+			});
+		});
