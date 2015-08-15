b0f743c19bb59e683a3907a3878575e7bd0fedb6
Miguel <migueog@gmail.com>
2015-08-15 16:47:19 -0400

    COC & Fixes
    - Fixed menu bug
    - Added COC
    - Added Data of conference
    - Changed favicon

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
-//Trim the fat
-
-		$(document).ready(function() {
-		    $("#homeButton").click(function() {
-		        scrollFunction("main");
-		    });
-		    $("#aboutButton").click(function() {
-		        scrollFunction("about");
-		    });
-		    $("#eventButton").click(function() {
-		        scrollFunction("about");
-		    });
-		    $("#contactButton").click(function() {
-		        scrollFunction("contact");
-		    });
-
-
-		});
-
-		function scrollFunction(divName) {
-		    divToScrollTo = "#".concat(divName)
-		    $('html, body').animate({
-		        scrollTop: $(divToScrollTo).offset().top - 50
-		    }, 1000);
+$(document).ready(function () {
+	$("#homeButton").click(function () {
+		scrollFunction("main");
+	});
+	$("#aboutButton").click(function () {
+		scrollFunction("about");
+	});
+	$("#eventButton").click(function () {
+		scrollFunction("about");
+	});
+	$("#cocButton").click(function () {
+		scrollFunction("coc");
+	});
+	$("#contactButton").click(function () {
+		scrollFunction("contact");
+	});
+	$(".navbar-nav li a").click(function (event) {
+		// check if window is small enough so dropdown is created
+		var toggle = $(".navbar-toggle").is(":visible");
+		if (toggle) {
+			$(".navbar-collapse").collapse('hide');
 		}
+	});
+});
 
-
-
+function scrollFunction(divName) {
+	divToScrollTo = "#".concat(divName)
+	$('html, body').animate({
+		scrollTop: $(divToScrollTo).offset().top - 50
+	}, 1000);
+}
